4a33f03794157342a221538cf4a3763d6372fced
wersosnwsk <wersosnwsk@gmail.com>
2024-08-18 14:41:59 +0200

    Solved problems: 1-2

---
+++ easy/sql-50/1-products.sql
@@ -0,0 +1,5 @@
+---1. Recyclable and Low Fat Products 
+---Write a solution to find the ids of products that are both low fat and recyclable.
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y';
