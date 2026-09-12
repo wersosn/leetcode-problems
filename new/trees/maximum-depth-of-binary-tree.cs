0aeea8b26cd9c2f738309973e36655f7aace5990
wersosn <wersosnwsk@gmail.com>
2026-09-12 12:25:07 +0200

    trees: maximum depth of binary tree

--- new/trees/maximum-depth-of-binary-tree.cs
+++ new/trees/maximum-depth-of-binary-tree.cs
@@ -1,5 +1,5 @@
 // Pattern: Depth-first search (DFS) or recursion
-// When to use: When you want to invert a binary tree
+// When to use: When you want to find the maximum depth of a binary tree
 // Complexity: O(n) time, O(h) space, where n is the number of nodes in the tree and h is the height of the tree (due to recursion stack)
 
 /**
@@ -16,19 +16,14 @@
  * }
  */
 public class Solution {
-    public TreeNode InvertTree(TreeNode root) {
+    public int MaxDepth(TreeNode root) {
         if(root == null) {
-            return null;
+            return 0;
         }
 
-        TreeNode tmp = root.left;
-        root.left = root.right;
-        root.right = tmp;
-
-        InvertTree(root.left);
-        InvertTree(root.right);
-
-        return root;
+        int leftDepth = MaxDepth(root.left);
+        int rightDepth = MaxDepth(root.right);
+        return Math.Max(leftDepth, rightDepth) + 1;
     }
 }
 
@@ -40,15 +35,11 @@ class Program
         Solution solution = new Solution();
         
         //Case 1:
-        var result1 = solution.InvertTree(new TreeNode(4, new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(7, new TreeNode(6), new TreeNode(9))));
+        var result1 = solution.MaxDepth(new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7))));
         Console.WriteLine("Result for case 1: " + result1);
 
         //Case 2:
-        var result2 = solution.InvertTree(new TreeNode(2, new TreeNode(1), new TreeNode(3)));
+        var result2 = solution.MaxDepth(new TreeNode(1, null, new TreeNode(2)));
         Console.WriteLine("Result for case 2: " + result2);
-
-        //Case 3:
-        var result3 = solution.InvertTree(null);
-        Console.WriteLine("Result for case 3: " + result3);
     }
 }
