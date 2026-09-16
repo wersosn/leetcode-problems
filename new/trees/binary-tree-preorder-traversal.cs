e06ce124748efbeb3b5616fe7a7ad6feaa6a6501
wersosn <wersosnwsk@gmail.com>
2026-09-16 10:29:47 +0200

    trees: binary tree preorder traversal

--- new/trees/binary-tree-preorder-traversal.cs
+++ new/trees/binary-tree-preorder-traversal.cs
@@ -1,5 +1,5 @@
 // Pattern: Iterative depth-first search (DFS) with an explicit stack
-// When to use: To traverse a binary tree in inorder: left subtree, root, right subtree
+// When to use: To traverse a binary tree in preorder: root, left subtree, right subtree
 // Complexity: O(n) time and O(h) space, where n is the number of nodes and h is the tree height
 
 /**
@@ -16,22 +16,24 @@
  * }
  */
 public class Solution {
-    public IList<int> InorderTraversal(TreeNode root) {
+    public IList<int> PreorderTraversal(TreeNode root) {
         if(root == null) {
             return new List<int>();
         }
 
         IList<int> result = new List<int>();
         Stack<TreeNode> stack = new Stack<TreeNode>();
-        TreeNode current = root;
-        while(current != null || stack.Count > 0) {
-            while(current != null) {
-                stack.Push(current);
-                current = current.left;
-            }
-            current = stack.Pop();
+        stack.Push(root);
+
+        while(stack.Count > 0) {
+            TreeNode current = stack.Pop();
             result.Add(current.val);
-            current = current.right;
+            if(current.right != null) {
+                stack.Push(current.right);
+            }
+            if(current.left != null) {
+                stack.Push(current.left);
+            }
         }
         return result;
     }
@@ -45,19 +47,19 @@ class Program
         Solution solution = new Solution();
 
         //Case 1:
-        var result1 = solution.InorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3))));
+        var result1 = solution.PreorderTraversal(new TreeNode(1, null, new TreeNode(2, new TreeNode(3))));
         Console.WriteLine("Result for case 1: " + result1);
 
         //Case 2:
-        var result2 = solution.InorderTraversal(new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(6), new TreeNode(7))), new TreeNode(3, new TreeNode(8, new TreeNode(9)))));
+        var result2 = solution.PreorderTraversal(new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5, new TreeNode(6), new TreeNode(7))), new TreeNode(3, new TreeNode(8, new TreeNode(9)))));
         Console.WriteLine("Result for case 2: " + result2);
 
         //Case 3:
-        var result3 = solution.InorderTraversal(null);
+        var result3 = solution.PreorderTraversal(null);
         Console.WriteLine("Result for case 3: " + result3);
 
         //Case 4:
-        var result4 = solution.InorderTraversal(new TreeNode(1));
+        var result4 = solution.PreorderTraversal(new TreeNode(1));
         Console.WriteLine("Result for case 4: " + result4);
     }
 }
